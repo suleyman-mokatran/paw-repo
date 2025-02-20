6a41b29961b5c200bef586e641fb480567d47e03
SULEYMAN <10121261@mu.edu.lb>
2025-02-20 17:32:12 +0200

    first push

---
+++ paw/src/test/java/com/PAWCOMPANY/paw/PawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PAWCOMPANY.paw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
