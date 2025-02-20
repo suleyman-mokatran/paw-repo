62d4e6902f55e5e4bfea2843b5ee00de553d9b9e
SULEYMAN <10121261@mu.edu.lb>
2025-02-20 22:39:06 +0200

    d

--- paw/src/main/java/com/PAWCOMPANY/paw/Models/AppUser.java
+++ paw/src/main/java/com/PAWCOMPANY/paw/Models/AppUser.java
@@ -16,7 +16,7 @@ public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int AppUserId;
-////test alusdfgfhjykuilkuyjthrgfeergthyjuk
+
     String firstname;
     String lastname;
     Date BirthDate;
